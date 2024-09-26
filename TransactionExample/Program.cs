f7d6dbb896a764c78a443431ab7e2978a9675a15
Jafar Mousavi <j.mosavi2@gmail.com>
2024-09-26 14:58:40 +0330

    feat: 🎸 Adding BeginTransaction Sample

--- TransactionExample/Program.cs
+++ TransactionExample/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Data.SqlClient;
 
 namespace TransactionExample
 {
@@ -9,6 +7,78 @@ namespace TransactionExample
     {
         static void Main(string[] args)
         {
+            var connection = new SqlConnectionStringBuilder();
+            connection.DataSource = @".\sql2017";
+            connection.InitialCatalog = "Chapyar_Test";
+            connection.UserID = "user";
+            connection.Password = "";
+
+            PrepareTestTable(connection.ConnectionString);
+
+            var testBeginTransaction = new BeginTransactionSample(connection.ConnectionString);
+            testBeginTransaction.StartTest();
+
+            Console.ReadKey();
+        }
+
+        private static void PrepareTestTable(string connectionString)
+        {
+            var sqlcon = new SqlConnection(connectionString);
+            var command = new SqlCommand();
+            command.CommandText = "DROP TABLE test; create table test(id int, name varchar(1000));";
+            command.Connection = sqlcon;
+            try
+            {
+                sqlcon.Open();
+                command.ExecuteScalar();
+            }
+            finally
+            {
+                sqlcon.Close();
+            }
+        }
+    }
+
+    class TransactionScopeSample
+    {
+
+    }
+
+    class BeginTransactionSample
+    {
+        private readonly string connectionString;
+        public BeginTransactionSample(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public void StartTest()
+        {
+            using (var sqlcn = new SqlConnection(connectionString))
+            {
+                sqlcn.Open();
+                using (var transaction = sqlcn.BeginTransaction())
+                {
+                    try
+                    {
+                        var command = new SqlCommand();
+                        command.CommandText = "INSERT INTO test (id, name) VALUES (5000, 'Test BeginTransaction statement')";
+                        command.Connection = sqlcn;
+                        command.Transaction = transaction;
+                        command.ExecuteNonQuery();
+                        transaction.Commit();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("There is an error:\r\n {0}", e.Message);
+                        transaction.Rollback();
+                    }
+                    finally
+                    {
+                        sqlcn.Close();
+                    }
+                }
+            }
         }
     }
 }
